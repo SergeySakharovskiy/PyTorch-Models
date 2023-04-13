3b3b1a2ae03647753d67ac43218b3bb173e8aa2d
SergeySakharovskiy <promydia@gmail.com>
2023-04-12 22:00:14 -0400

    2023-04-12 initiaal vgg.py v.3.

--- src/vision_models/vgg.py
+++ src/vision_models/vgg.py
@@ -15,4 +15,6 @@ VGG = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 
 
 class VGG(nn.Module):
     def __init__(self, ):
+        super().__init__()
         self.a = None
+        
