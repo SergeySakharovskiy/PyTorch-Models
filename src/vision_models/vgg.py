8f2ab6bea01fc33514fd524fdd253a00d0e841d0
SergeySakharovskiy <promydia@gmail.com>
2023-04-16 22:12:25 -0400

    2023-04-16 initial vgg.py v.4.

--- src/vision_models/vgg.py
+++ src/vision_models/vgg.py
@@ -11,10 +11,12 @@ import torch.nn.functional as F
 
 # VGG 13
 VGG = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
-# Dense layears are 4096 -> 4096 -> 1000 (in our case 3)
+
+
+# Dense layers are 4096 -> 4096 -> 1000 (in our case 3)
 
 class VGG(nn.Module):
     def __init__(self, ):
         super().__init__()
         self.a = None
-        
+        self.b = None
